891480ef15e5287b9aed63d0058b97e5bd598360
Aldo0501 <mymalfa@gmail.com>
2023-04-07 20:20:10 -0700

    Ruta de registro

--- src/routes/newUser.mjs
+++ src/routes/newUser.mjs
@@ -1,6 +1,8 @@
 import express from 'express'
+import bcrypt from 'bcrypt';
 import executeQuery from '../Database/databaseFunctions.mjs'
 const router = express.Router()
+const saltRounds = 10;
 
 router.post('/newUser', async (req, res) => {
   console.log(req.body)
@@ -8,15 +10,28 @@ router.post('/newUser', async (req, res) => {
   const sql = `INSERT INTO users (email, key_password, user_name, last_name, nick_name)
   VALUES (?, ?, ?, ?, ?);`
 
-  //TODO: iterar las llaves del req.body para insertar el valor de las llaves en un arreglo y pasar el arrelgo como el segundo parametro de la funcion executeQuer
   // const data = ['Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006']
 
-  await executeQuery(sql, data)
+  const data = Object.values(req.body)
+
+  bcrypt.hash(data[1], saltRounds, async function(err, hash) {
+    if(err){
+      console.log(err)
+      res.status(500)
+    }
+
+    data[1] = hash
+
+   const results = await executeQuery(sql, data)
     .then(res.json({message: "Usuario registrado correctamente"}))
     .catch((err) => {
       console.log(err)
       res.status(500)
     })
+    console.log(results.insertId)
+});
+
+  
 })
 
 export default router
